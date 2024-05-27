f7b75e8914efa3a41d193aaeb9a6a0f4812e4936
Akshith268 <akshith268@gmail.com>
2024-05-27 16:29:53 +0530

    seven commit

--- app.py
+++ app.py
@@ -38,4 +38,6 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable if available
+    app.run(host='0.0.0.0', port=port, debug=True)
+ 
